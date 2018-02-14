5da70ba4dcbbfcecaf7026878827dd51e4ae7017
Vish Sivaraman <vishsivaraman@outlook.com>
2018-02-14 17:04:11 -0600

    Adding mok to unit tests

--- IndexCard.Tests/Controllers/HomeControllerTest.cs
+++ IndexCard.Tests/Controllers/HomeControllerTest.cs
@@ -6,12 +6,25 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IndexCard;
 using IndexCard.Controllers;
+using Rhino.Mocks;
+using IndexCard.Entities;
+using IndexCard.Models;
 
 namespace IndexCard.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+
+        private IIndexCardRepo mokIndexCardRepo;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mokIndexCardRepo = MockRepository.GenerateStub<IIndexCardRepo>();
+        }
+
+
         [TestMethod]
         public void Index()
         {
@@ -50,5 +63,26 @@ namespace IndexCard.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void GetQuestionAnswer()
+        {
+            var requiredKvp = new QnA()
+            {
+                Question = "TestQuestion",
+                Answer = "TestAnswer"
+            };
+
+            // Arrange
+            mokIndexCardRepo.Stub(m => m.GetQuestionAnswer()).Return(new KeyValuePair<string, string>(requiredKvp.Question, requiredKvp.Answer));
+
+            var controller = new QnAsController(mokIndexCardRepo);
+            ViewResult result = controller.IndexCardDisplay();
+            var model = result.Model as QnA;
+
+            Assert.AreEqual(requiredKvp.Question, model.Question);
+            Assert.AreEqual(requiredKvp.Answer, model.Answer);
+        }
+
     }
 }
